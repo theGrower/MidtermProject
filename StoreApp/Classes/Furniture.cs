cb5b7a161668ddb436ce9ad66c5cbc489f46a616
Jeff Stewart <jeff.stewart@protonmail.com>
2017-04-11 20:41:50 -0400

    Created Furniture class
    added the details

--- StoreApp/Classes/Furniture.cs
+++ StoreApp/Classes/Furniture.cs
@@ -8,5 +8,38 @@ namespace StoreApp.Classes
 {
     public class Furniture
     {
+        private decimal price;
+        private string description;
+        private int modelNum;
+
+        public Furniture()
+        {
+            this.price = 0;
+            this.description = null;
+            this.modelNum = 0;
+        }
+
+        public Furniture(decimal price, string descriptions)
+        {
+            this.price = this.price;
+            this.description = this.description;
+            this.modelNum = this.modelNum;
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public int ModelNum
+        {
+            get { return modelNum; }
+            set { modelNum = value; }
+        }
     }
 }

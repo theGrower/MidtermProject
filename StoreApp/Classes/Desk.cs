873d2d1a807dea21720756457572d290fa69dfab
ghouck <ghouck@steelcase.com>
2017-04-11 20:42:55 -0400

    Desk class created
    Desk class created

---
+++ StoreApp/Classes/Desk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Classes
+{
+    class Desk
+    {
+        public class Desking
+        {
+            private 
+        }
+    }
+}

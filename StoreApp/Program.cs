86ced9cceba7e05062a4bb8bfcfade08eeb46563
Jeff Stewart <jeff.stewart@protonmail.com>
2017-04-12 22:22:24 -0400

    Moved greeting to main program file
    see above

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -19,7 +19,12 @@ namespace StoreApp
 
         static void Greeting()
         {
-
+            Console.WriteLine("Hello. Thank you for visiting Steelcase.");
+            Console.WriteLine("Our store offers office furniture designed with you in mind.");
+            Console.WriteLine("Choose from our selection of desks, files, seating, and tables to custom design your office");
+            Console.WriteLine("Let's get started!");
+            Console.WriteLine("What type of furniture would you like to browse first?");
+            Console.Write("Please type \"desks\", \"files\", \"seating\", or \"table\": ");
         }
 
         static void CreateArrays()
